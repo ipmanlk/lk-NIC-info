91a76eae0ddaa0c5a6c7402e1818cb61fe93aa1f
ipman98 <navilk@zoho.com>
2018-06-16 17:07:09 +0530

    moved: loadMySite() to common.js

--- www/js/common.js
+++ www/js/common.js
@@ -1,7 +1,13 @@
 function isNotNull(str) {
-    if (str!==null && str!=='') {
+    if (str !== null && str !== '') {
         return true;
     } else {
         return false;
     }
+}
+
+//load in device system browser
+function loadMySite() {
+    var ref = cordova.InAppBrowser.open('https://www.navinda.xyz', '_system', 'location=yes');
+    ref.close();
 }
